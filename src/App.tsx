7db905de755145b44596e64b5b8defec1a668c5b
Minchan <98478661+itscold96@users.noreply.github.com>
2025-02-27 08:36:10 +0900

    fix: useSuspenseQuery와 React.lazy가 함께 사용되고 있는 부분 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { lazy, Suspense } from 'react';
-const LazyAdvertisement = lazy(() => import('./components/advertisement/Advertisement'));
+import { Suspense } from 'react';
+import Advertisement from './components/advertisement/Advertisement';
 
 const BANNER_LARGE = 'PUBLIC_TEST_UNIT_ID_375_80';
 
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div style={{ height: '80px', width: '375px' }}>
       <Suspense fallback={<div>Loading...</div>}>
-        <LazyAdvertisement unitId={BANNER_LARGE} />
+        <Advertisement unitId={BANNER_LARGE} />
       </Suspense>
     </div>
   );
